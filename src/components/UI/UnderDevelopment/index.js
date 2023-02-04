64b76245b06671e03e09744224cc8ba0b938abf3
Ahmed Othman <ahmed.othman.ghazzawi@gmail.com>
2023-02-04 12:30:08 +0200

    enahnced under development mobile view

--- src/components/UI/UnderDevelopment/index.js
+++ src/components/UI/UnderDevelopment/index.js
@@ -2,7 +2,8 @@ import styles from './index.module.scss';
 const UnderDevelopment = () => {
   return (
     <div className={styles['container']}>
-      <h1>Under Development 🧑🏽‍💻</h1>
+      <h1>🧑🏽‍💻</h1>
+      <h1>Under Development</h1>
     </div>
   );
 };
